cd133ed99f8c75886a17651c8db000200bce3ad6
Reshma <reshmaranjucv@gmail.com>
2026-01-27 17:38:18 +0530

    weather splash screen and weather api implementation

--- lib/View/Splash/splash_screen.dart
+++ lib/View/Splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/Resources/Colors/colors.dart';
 import 'package:weather_app/Resources/Images/image_assets.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,9 +12,13 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Center(
-        child: Image.asset(ImageAssets.nightStarRain,height: 200,width: 200),
+    return  Container(
+      decoration: AppColors.buildGradientBoxDecoration(),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Image.asset(ImageAssets.nightStarRain,height: 200,width: 200),
+        ),
       ),
     );
   }
