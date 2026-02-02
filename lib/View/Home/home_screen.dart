ab273fafe82f8728f83343e187d45036f62a5fea
Reshma <reshmaranjucv@gmail.com>
2026-02-02 14:12:16 +0530

    home screen done

--- lib/View/Home/home_screen.dart
+++ lib/View/Home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:weather_app/Resources/Colors/colors.dart';
 import 'package:weather_app/View/Home/components/app_bar.dart';
 import 'package:weather_app/View/Home/components/container_list.dart';
+import 'package:weather_app/View/Home/components/hours_list.dart';
 import 'package:weather_app/View/Home/components/info_card.dart';
 import 'package:weather_app/View/NextDays/next_14_days.dart';
 import 'package:weather_app/ViewModel/Controllers/home_controller.dart';
@@ -26,41 +27,45 @@ class _HomeScreenState extends State<HomeScreen> {
         body: SafeArea(
           child:Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-             children: [
-              const SizedBox(height: 2),
-              const CustomAppBar(),
-              const SizedBox(height:2),
-              Location(),
-              Hero(tag: 'TAG', child: Material(
-                color: Colors.transparent,
-                child: InfoCard(),
-              )),
-              ContainerList(),
-              const SizedBox(height: 4),
-              GestureDetector(
-                onTap: ()=>Get.to(()=>const NextDays()),
-                child: const Padding(padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  children: [
-                    Text('Today',style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25
-                    ),),
-                    Spacer(),
-                    Text(
-                      'Next 14 Days',
-                      style: TextStyle(
-                        color: Colors.blue,
+            child: SingleChildScrollView(
+              child: Column(
+               children: [
+                const SizedBox(height: 2),
+                const CustomAppBar(),
+                const SizedBox(height:2),
+                Location(),
+                Hero(tag: 'TAG', child: Material(
+                  color: Colors.transparent,
+                  child: InfoCard(),
+                )),
+                ContainerList(),
+                const SizedBox(height: 4),
+                GestureDetector(
+                  onTap: ()=>Get.to(()=>const NextDays()),
+                  child: const Padding(padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: [
+                      Text('Today',style: TextStyle(
+                        color: Colors.black,
                         fontWeight: FontWeight.bold,
-                        fontSize: 18
-                        ),
-                        )
-                  ],
+                        fontSize: 25
+                      ),),
+                      Spacer(),
+                      Text(
+                        'Next 14 Days',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18
+                          ),
+                          )
+                    ],
+                  ),
+                )
                 ),
-              ))
-             ],
+                HoursList()
+               ],
+              ),
             ),
             ) ,
         ),
